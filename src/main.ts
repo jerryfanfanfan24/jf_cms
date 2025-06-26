060a28d5701de63173c9f0203817afd58b53af9d
jf <peng07613@gmail.com>
2025-06-26 23:20:32 +0800

    自动lint校验

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,14 @@ import App from './App.vue'
 import router from './router'
 import pinia from './stores'
 import registerIcons from './global/register-icons'
+import useLoginStore from './stores/login/login';
+import registerStore from './stores';
+
+const app = createApp(App)
 
 
+
+app.use(registerIcons)
 
 
 /* app.use(pinia)
